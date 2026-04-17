a034b38b502e735fcfd3e9cf92a2dec86b6a388d
incoval <102046584+incoval@users.noreply.github.com>
2026-04-17 17:30:24 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: '/acheter-ou-louer-simu/',
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+export default defineConfig({
+  base: "/acheter-ou-louer-simu/",
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
-
-
+});
