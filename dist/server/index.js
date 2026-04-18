fd7d30f839cf06a4631d3318ec68a10434cfc1a9
Valentin Herrero <valentinherrero@Host-002.lan>
2026-04-18 13:53:35 +0200

    Try basename instead of basepath

--- dist/server/index.js
+++ dist/server/index.js
@@ -1,4 +1,4 @@
-import { a1, a2 } from "./assets/worker-entry-HwQKDRKc.js";
+import { a1, a2 } from "./assets/worker-entry-BXBTPiNK.js";
 import "node:events";
 import "node:async_hooks";
 import "node:stream/web";
