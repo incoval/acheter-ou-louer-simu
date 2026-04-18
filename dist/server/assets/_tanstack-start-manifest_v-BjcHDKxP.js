ecf2b34ab298501e8691eac62478bdbe4609d52c
Valentin Herrero <valentinherrero@Host-002.lan>
2026-04-18 12:54:53 +0200

    Rebuild dist with fixed UI

---
+++ dist/server/assets/_tanstack-start-manifest_v-BjcHDKxP.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/Users/valentinherrero/Desktop/acheter-ou-louer-simu/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-Cc8ILZrP.js"] }, "/": { filePath: "/Users/valentinherrero/Desktop/acheter-ou-louer-simu/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-CjomJpB6.js"] } }, clientEntry: "/assets/index-Cc8ILZrP.js" });
+export {
+  tsrStartManifest
+};
